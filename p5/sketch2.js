02e36cb51b55ab9fff4e1385543b3c554fc3e040
randomArt <imup@qq.com>
2026-09-16 19:09:14 +0800

    更新 sketch2.js

--- p5/sketch2.js
+++ p5/sketch2.js
@@ -1,67 +1,147 @@
-var particles_a = [];
-var particles_b = [];
-var particles_c = [];
-var nums =200;
+var particles = [];
+var nums;
+var particleDensity = 4000;
 var noiseScale = 800;
+var maxLife = 10;
+var  simulationSpeed = 0.2;
+var fadeFrame = 0;
+var backgroundColor;
+var visualMode = 2;
+var numModes = 4;
+var invertColors = false;
 
-function setup(){
-  createCanvas(windowWidth, windowHeight);
-  background(21, 8, 50);
-  for(var i = 0; i < nums; i++){
-    particles_a[i] = new Particle(random(0, width),random(0,height));
-    particles_b[i] = new Particle(random(0, width),random(0,height));
-    particles_c[i] = new Particle(random(0, width),random(0,height));
-  }
+function setup() {
+   nums = windowWidth * windowHeight / particleDensity;
+   backgroundColor = color(20, 20, 20);
+   createCanvas(windowWidth, windowHeight);
+   background(backgroundColor);
+   for (var i = 0; i < nums; i++) {
+      particles[i] = new Particle();
+   }
 }
 
-function draw(){
-  noStroke();
-  smooth();
-    for(var i = 0; i < nums; i++){
-    var radius = map(i,0,nums,1,2);
-    var alpha = map(i,0,nums,0,250);
-
-    fill(69,33,124,alpha);
-    particles_a[i].move();
-    particles_a[i].display(radius);
-    particles_a[i].checkEdge();
-
-    fill(7,153,242,alpha);
-    particles_b[i].move();
-    particles_b[i].display(radius);
-    particles_b[i].checkEdge();
-
-    fill(255,255,255,alpha);
-    particles_c[i].move();
-    particles_c[i].display(radius);
-    particles_c[i].checkEdge();
-  }  
+function draw() {
+   noStroke();
+   
+   ++fadeFrame;
+   if (fadeFrame % 5 == 0) {
+      if (invertColors) {
+         blendMode(ADD);
+      } else {
+         blendMode(DIFFERENCE);
+      }
+      fill(1, 1, 1);
+      rect(0, 0, width, height);
+      
+      if (invertColors) {
+         blendMode(DARKEST);
+      } else {
+         blendMode(LIGHTEST);
+      }
+      fill(backgroundColor);
+      rect(0, 0, width, height);
+   }
+   
+   blendMode(BLEND);
+   smooth();
+   for (var i = 0; i < nums; i++) {
+      var iterations = map(i, 0, nums, 5, 1);
+      var radius = map(i, 0, nums, 2, 6);
+      
+      particles[i].move(iterations);
+      particles[i].checkEdge();
+      
+      var alpha = 255;
+      var particleColor;
+      var fadeRatio;
+      fadeRatio = min(particles[i].life * 5 / maxLife, 1);
+      fadeRatio = min((maxLife - particles[i].life) * 5 / maxLife, fadeRatio);
+      var colorCase = visualMode;
+      if (visualMode == 0) {
+         colorCase = int(particles[i].pos.x / width * 3) + 1;
+      }
+      switch(colorCase) {
+         case 1:
+         var lifeRatioGrayscale = min(255, (255 * particles[i].life / maxLife) + red(backgroundColor));
+         particleColor = color(lifeRatioGrayscale, alpha * fadeRatio);
+         break;
+         case 2:
+         particleColor = particles[i].color;
+         break;
+         case 3:
+         particleColor = color(blue(particles[i].color) + 70, green(particles[i].color) + 20, red(particles[i].color) - 50);
+         break;
+      }
+      if (invertColors) {
+         particleColor = color(255 - red(particleColor), 255 - green(particleColor), 255 - blue(particleColor));
+      }
+      fill(red(particleColor), green(particleColor), blue(particleColor), alpha * fadeRatio);
+      particles[i].display(radius);
+   }
 }
 
+function Particle() {
+   this.vel = createVector(0, 0);
+   this.pos = createVector(random(0, width), random(0, height));
+   this.life = random(0, maxLife);
+   this.flip = int(random(0, 2)) * 2 - 1;
+   var randColor = int(random(0, 3));
+   switch(randColor) {
+      case 0:
+      this.color = color(110, 57, 204);
+      break;
+      case 1:
+      this.color = color(7, 153, 242);
+      break;
+      case 2:
+      this.color = color(255, 255, 255);
+      break;
+   }
+   
+   this.move = function(iterations) {
+      if ((this.life -= 0.01666) < 0)
+      this.respawn();
+      while (iterations > 0) {
+         var angle = noise(this.pos.x/noiseScale, this.pos.y/noiseScale)*TWO_PI*noiseScale*this.flip;
+         this.vel.x = cos(angle);
+         this.vel.y = sin(angle);
+         this.vel.mult(simulationSpeed);
+         this.pos.add(this.vel);
+         --iterations;
+      }
+   }
+   
+   this.checkEdge = function() {
+      if (this.pos.x > width || this.pos.x < 0 || this.pos.y > height || this.pos.y < 0) {
+         this.respawn();
+      }
+   }
+   
+   this.respawn = function() {
+      this.pos.x = random(0, width);
+      this.pos.y = random(0, height);
+      this.life = maxLife;
+   }
+   
+   this.display = function(r) {
+      ellipse(this.pos.x, this.pos.y, r, r);
+   }
+}
 
-function Particle(x, y){
-  this.dir = createVector(0, 0);
-  this.vel = createVector(0, 0);
-  this.pos = createVector(x, y);
-  this.speed = 0.4;
+function advanceVisual() {
+   visualMode = ++visualMode % numModes;
+   if (visualMode == 0) {
+      invertColors = !invertColors;
+      backgroundColor = invertColors ? color(235, 235, 235) : color(20, 20, 20);
+   }
+   noiseSeed(random()*Number.MAX_SAFE_INTEGER);
+   background(backgroundColor);
+   for (var i = 0; i < nums; i++) {
+      particles[i].respawn();
+      particles[i].life = random(0, maxLife);
+   }
+}
 
-  this.move = function(){
-    var angle = noise(this.pos.x/noiseScale, this.pos.y/noiseScale)*TWO_PI*noiseScale;
-    this.dir.x = cos(angle);
-    this.dir.y = sin(angle);
-    this.vel = this.dir.copy();
-    this.vel.mult(this.speed);
-    this.pos.add(this.vel);
-  }
-
-  this.checkEdge = function(){
-    if(this.pos.x > width || this.pos.x < 0 || this.pos.y > height || this.pos.y < 0){
-      this.pos.x = random(50, width);
-      this.pos.y = random(50, height);
-    }
-  }
-
-  this.display = function(r){
-    ellipse(this.pos.x, this.pos.y, r, r);
-  }
+function touchStarted(){
+  advanceVisual();
 }
