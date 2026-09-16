cfb85ef4cde4d18d5daf4aec5e3c93e416fed224
randomArt <imup@qq.com>
2026-09-16 19:07:16 +0800

    更新 sketch3.js

--- p5/sketch3.js
+++ p5/sketch3.js
@@ -1,22 +1,45 @@
-let t = 0;
+let _bgWidth;
+let _minWidth;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(10, 10);
-  translate(width / 2, height / 2);
-  for (let i = 0; i < 20; i++) {
-    let angle = t + i * 0.3;
-    let r = 50 + i * 15;
-    fill(255 - i * 10, 100 + i * 5, 200);
-    noStroke();
-    ellipse(cos(angle) * r, sin(angle) * r, 20, 20);
+function setup(){
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  setAttributes("alpha", false);
+  _bgWidth = min(width, height);
+  _minWidth = _bgWidth * 0.95
+  colorMode(HSB, 360, 100, 100, 255);
+  noStroke();
+  background(255);
+  let r = random(_minWidth / 3, _minWidth / 0.5);
+  let numShape = ((10 * _minWidth / 2 / r))**1.5 / 3;
+  for (let i = 0; i < numShape; i++) {
+    drawShape(r);
   }
-  t += 0.02;
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+function drawShape(r) {
+  let numCorner = 64;
+  let x = random(-_minWidth/2, _minWidth/2);
+  let y = random(-_minWidth/2, _minWidth/2);
+  let ang = random(2*PI);
+  let h = random(360);
+  push();
+  translate(x, y);
+  rotate(ang);
+  beginShape();
+  for (let i = 0; i < numCorner; i++) {
+  if (i == 0) { 
+  fill(h, 100, 100); 
+}
+  if (i != 0) { 
+  fill(h, 0, 100, 0); 
+}
+  vertex(r*cos(2*PI/numCorner*i), r*sin(2*PI/numCorner*i));
+  }
+  endShape();
+  pop();
+ 
+}
+
+function touchStarted(){
+  setup();
 }
