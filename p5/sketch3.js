5288999096ddcab46c975242157e5105b2f09c40
randomArt <imup@qq.com>
2026-09-18 14:13:39 +0800

    更新 sketch3.js

--- p5/sketch3.js
+++ p5/sketch3.js
@@ -1,5 +1,38 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(255);
+  noFill();
+  stroke(255);
+  frameRate(30);
+  colorMode(HSB, 360, 100, 100, 360);
+}
+function draw() {
+  fill(270, 0, 100, 50);
+  noStroke();
+  rect(0, 0, width, height);
+  noFill();
+  strokeWeight(1.5);
+  for (let i = 0; i <= width / 2; i++) {
+    const hue = map(i, 0, width / 2, 0, 360);
+    stroke(hue, 100, 100, 128);
+    beginShape();
+    for (let x = -10; x < width + 21; x += 10) {
+      const n = noise(x * 0.001, frameCount * 0.008, i * 0.02);
+      const y = map(n, 0, 1, 0, height);
+      curveVertex(x, y);
+    }
+    endShape();
+  }
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+
+/*
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
   noStroke();
   noLoop();
 }
@@ -86,3 +119,5 @@ function makeRect(x, y, w, h) {
     }
   }
 }
+
+*/
